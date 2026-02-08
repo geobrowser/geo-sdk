e21d652ce9e727291a08ef0e6ac024d136066025
Nik Graf <nik@nikgraf.com>
2026-02-08 13:00:54 +0100

    remove Graph.createSpace

--- src/graph/index.ts
+++ src/graph/index.ts
@@ -7,7 +7,6 @@ export * from './create-entity.js';
 export * from './create-image.js';
 export * from './create-property.js';
 export * from './create-relation.js';
-export * from './create-space.js';
 export * from './create-type.js';
 export * from './delete-entity.js';
 export * from './delete-relation.js';
